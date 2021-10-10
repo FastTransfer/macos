683e17e80d99ef7de0fedbfce639b9bc9fc36000
Mikołaj Pich <m.pich@outlook.com>
2021-10-10 17:34:04 +0200

    Initial Commit

---
+++ FastTransfer/FastTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTransferApp.swift
+//  FastTransfer
+//
+//  Created by Mikołaj on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FastTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
